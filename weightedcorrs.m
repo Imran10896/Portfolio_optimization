58b241c5f92e02304bea2f50ddeb3ad97a5fd81d
IMRAN ANSARI <95048782+Imran10896@users.noreply.github.com>
2024-02-19 21:19:13 +0530

    Update weightedcorrs.m

--- weightedcorrs.m
+++ weightedcorrs.m
@@ -1,7 +1,7 @@
-function R = weightedcorrs(Y, w)
+function R = weightedcorrs(Y)
 % Calculates the Pearson Weighted Correlation coefficients matrix.
 %
-%   R = weightedcorrs(Y, w)
+%   R = weightedcorrs(Y, w, theta)
 %
 %   This function computes the weighted correlation coefficients matrix based on the input data matrix Y
 %   and the vector of weights w.
@@ -10,6 +10,7 @@ function R = weightedcorrs(Y, w)
 %   - Y: Data matrix with dimensions (dt, N), where dt is the number of observations in the window and
 %        N is the number of variables.
 %   - w: Vector of weights with dimensions (1, N).
+%   - theta: Parameter for exponential weights.
 %
 %   Output Argument:
 %   - R: Weighted correlation coefficients matrix with dimensions (N, N).
@@ -26,11 +27,24 @@ function R = weightedcorrs(Y, w)
 %   - The vector of weights w should have the same number of elements as the number of variables in Y.
 %
 %   Example:
-%   R = weightedcorrs(Y, w);
-%
+%   R = weightedcorrs(Y, theta);
 
 % Get the dimensions of the data matrix Y
 [dt, N] = size(Y);
+
+function w_exp = expweights(dt, theta)
+    % Calculate constant w0
+    w0 = (1 - exp(-1/theta)) / (1 - exp(-dt/theta));
+    
+    % Calculate exponential weights for Pearson correlation
+    w_exp(:,1) = w0 * exp(((1:dt) - dt) / theta);
+    
+    % Ensure sum of weights is 1
+    w_exp = w_exp / sum(w_exp);
+end
+
+% Call the expweights function
+w = expweights(dt, theta); 
 
 % Remove the weighted mean
 temp = Y - repmat(w' * Y, dt, 1);
